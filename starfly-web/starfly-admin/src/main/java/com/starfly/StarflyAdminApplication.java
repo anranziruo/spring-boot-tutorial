843a4e183b8f10f2e362849d9e14f0e0ae3a4ed6
zhangchao <1033573839@qq.com>
2023-08-10 01:25:41 +0800

    增加多模块开发

---
+++ starfly-web/starfly-admin/src/main/java/com/starfly/StarflyAdminApplication.java
@@ -0,0 +1,13 @@
+package com.starfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarflyAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarflyAdminApplication.class, args);
+	}
+
+}
