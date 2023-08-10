edbb22b93aea35f13440c8468664fcd3200f3e0a
zhangchao <1033573839@qq.com>
2023-08-10 10:57:25 +0800

    修改class名称

--- starfly-web/starfly-admin/src/main/java/com/starfly/StarflyAdminApplication.java
+++ starfly-web/starfly-admin/src/main/java/com/starfly/StarflyAdminApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StarflyAdminApplication {
+public class StarFlyAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StarflyAdminApplication.class, args);
+		SpringApplication.run(StarFlyAdminApplication.class, args);
 	}
 
 }
