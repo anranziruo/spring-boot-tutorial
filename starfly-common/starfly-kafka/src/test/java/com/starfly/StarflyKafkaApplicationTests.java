b244463adaf55904ab4955cde88ec6d378eb6135
zhangchao <1033573839@qq.com>
2023-08-11 20:02:31 +0800

    增加kafka

---
+++ starfly-common/starfly-kafka/src/test/java/com/starfly/StarflyKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarflyKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
